ae95da1a739eff46f46430fefbf13f9463ebc597
kellanliu188 <kellanliu188@gmail.com>
2024-06-27 11:45:18 -0700

    guess game complete

--- number_guesser/script.js
+++ number_guesser/script.js
@@ -3,4 +3,20 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+};
 
+function compareGuesses(humanGuess, computerGuess, targetNumber) {
+    humanDistance = Math.abs(targetNumber - humanGuess);
+    computerDistance = Math.abs(targetNumber - computerGuess)
+    if (humanDistance < computerDistance || humanDistance === computerDistance) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const updateScore = (winner) => winner === 'human' ? humanScore++ : computerScore++;
+
+const advanceRound = () => currentRoundNumber++;
